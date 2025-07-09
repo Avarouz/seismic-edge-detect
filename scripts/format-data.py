89b62fadc9a880631637e62d94014f76a9484b3d
Ava Rose <avarouzz@berkeley.edu>
2025-07-08 18:04:47 -0700

    fixing data issue

--- scripts/format-data.py
+++ scripts/format-data.py
@@ -1,8 +1,16 @@
 import os
+
 from utils.io import extract_data
 from utils.plot import plot_seismogram
+print(os.getcwd())
 
 print("Starting seismogram conversion...")
+
+import h5py
+f = h5py.File('../quakeflow_das/data/ridgecrest_north/ci37280444.h5', 'r')
+print(list(f.keys()))
+f.close()
+
 
 files = [
     "../quakeflow_das/data/ridgecrest_north/ci37280444.h5",
@@ -11,6 +19,10 @@ files = [
 
 # generate seismogram images
 for filepath in files:
+    print("Current working directory:", os.getcwd())
+    print("Trying to open:", filepath)
+    print("File exists:", os.path.exists(filepath))
+
     data, dt = extract_data(filepath)
     filename = os.path.splitext(os.path.basename(filepath))[0]
     outpath = f"results/seismograms/{filename}.png"
